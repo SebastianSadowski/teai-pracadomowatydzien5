f7d2ee9bee1656ad1e6e43608c061e82fc1df799
Sebastian Sadowski <sebastiansadowski95@interia.pl>
2020-05-31 23:24:08 +0200

    POBIERANIE INFO Z WEATHERSTACK PO NAZWIE MIASTA PODSTAWOWY CLIENT MODEL INFO Z  WATHERSTACK MODEL SKRÓCONYCH INFO DLA CLIENTA

---
+++ src/main/java/pl/sadowski/teaipracadomowatydzien5/TeaiPracadomowatydzien5Application.java
@@ -0,0 +1,13 @@
+package pl.sadowski.teaipracadomowatydzien5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiPracadomowatydzien5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaiPracadomowatydzien5Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sadowski/teaipracadomowatydzien5/TeaiPracadomowatydzien5ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sadowski.teaipracadomowatydzien5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiPracadomowatydzien5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
